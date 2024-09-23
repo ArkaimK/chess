734c58b029b57dfa54ab04e31ca2ca0b039631c7
ArkaimK <arkaim173@gmail.com>
2024-09-23 14:09:58 +0400

    Add files via upload

---
+++ chess_engine.py
@@ -0,0 +1,321 @@
+# gamestate, move log
+class Game_State():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR",],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP",],
+            ["--", "--", "--", "--", "--", "--", "--", "--",],
+            ["--", "--", "--", "--", "--", "--", "--", "--",],
+            ["--", "--", "--", "--", "--", "--", "--", "--",],
+            ["--", "--", "--", "--", "--", "--", "--", "--",],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP",],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR",],
+            ]
+        self.whitetomove = True
+        self.move_log = []
+        self.move_functions = {'P': self.pawnmoves, 'R': self.rookmoves, 'N': self.knightmoves, 
+                      'B': self.bishopmoves, 'Q': self.queenmoves, 'K': self.kingmoves}
+        self.whiteking_location = (7, 4)
+        self.blackking_location = (0, 4)
+        self.checkmate = False
+        self.stalemate = False
+
+        self.castle_log = []
+        self.queensidecastle_white = True
+        self.kingsidecastle_white = True
+        self.queensidecastle_black = True
+        self.kingsidecastle_black = True
+
+        # В мувлоге лежит дефолтное состояние доски как первый элемент
+    def make_move(self, move):
+        
+            self.board[move.first_row][move.first_column] = "--"
+            self.board[move.second_row][move.second_column] = move.movedpiece
+            #делает ход, меняя местами два кликнутых значения в board
+            if move.castling:                               #--------------------не работает------------------
+                print("ошибка не тут")
+                self.board[7][5] = self.board[7][7]
+                self.board[7][7] = '--'
+
+            self.move_log.append(move)
+            #В мувлог записывается класс мув (координаты первой, второй нажатых клеток, двинутой и съеденой фигуры)
+            self.whitetomove = not self.whitetomove
+
+            #отслеживание королей для проверки на шах и возможность рокировки
+            if move.movedpiece == "wK":
+                self.whiteking_location = (move.second_row, move.second_column)
+                self.queensidecastle_white = False
+                self.kingsidecastle_white = False
+            elif move.movedpiece == "bK":
+                self.blackking_location = (move.second_row, move.second_column)
+                self.queensidecastle_black = False
+                self.kingsidecastle_black = False
+                
+            #отслеживание ладей для проверки на возможность рокировки
+            if move.first_row == 7 and move.first_column == 0 and move.movedpiece == 'wR':
+                self.queensidecastle_white = False
+            if move.first_row == 7 and move.first_column == 7 and move.movedpiece == 'wR':
+                self.kingsidecastle_white = False
+            if move.first_row == 0 and move.first_column == 0 and move.movedpiece == 'bR':
+                self.queensidecastle_black = False
+            if move.first_row == 0 and move.first_column == 7 and move.movedpiece == 'bR':
+                self.kingsidecastle_black = False
+            
+            self.castle_log.append((self.queensidecastle_white, self.kingsidecastle_white, self.queensidecastle_black, self.kingsidecastle_black,))
+            #пополнение мувлога возможности рокировки
+
+            #промоушн пешки(тут можно реализовать вопрос к игроку во что превращать пешку, по умолчанию Q)
+            if (move.movedpiece == 'wP' and move.second_row == 0) or (move.movedpiece == 'bP' and move.second_row == 7):
+                self.board[move.second_row][move.second_column] = move.movedpiece[0]+'Q'
+    
+    def undo_move(self):
+        if len(self.move_log) != 0:
+            move = self.move_log.pop()
+            self.board[move.first_row][move.first_column] = move.movedpiece
+            self.board[move.second_row][move.second_column] = move.capturedpiece
+            #Выдергивает последний элемент в мувлоге(объект класс) и на основе него отменяет ход
+            if move.castling:                                                                           #--------------------------------------------
+                print("ошибка не тут")
+                self.board[7][7] = self.board[7][5]
+                self.board[7][5] = '--'
+
+            self.whitetomove = not self.whitetomove
+            # отмена координат королей
+            if move.movedpiece == "wK":
+                self.whiteking_location = (move.first_row, move.first_column)
+            elif move.movedpiece == "bK":
+                self.blackking_location = (move.first_row, move.first_column)
+            
+
+        if len(self.castle_log) > 1:
+            last = self.castle_log[-2]
+            self.queensidecastle_white = last[0]
+            self.kingsidecastle_white = last[1]
+            self.queensidecastle_black = last[2]
+            self.kingsidecastle_black = last[3]
+            del self.castle_log[-1]
+            #откат мувлога возможности рокировок и присвоение предыдущих значений
+
+    
+
+    def validmoves(self, possible_moves): #фильтрует ходы с возможных до валидных в переданном списке
+        moves = possible_moves
+        castlemoves = self.castlemoves(self.whiteking_location[0], self.whiteking_location[1]) if self.whitetomove else self.castlemoves(self.blackking_location[0], self.blackking_location[1])#рокировка не проходит проверку ниже, тк вызывает рекурсию, имеет проверку внутри себя
+        for i in range(len(moves)-1, -1, -1):
+            self.make_move(moves[i]) #Делает "фантомный ход" для каджого из возможного в списке
+            self.whitetomove = not self.whitetomove
+            if self.check():                      
+                moves.remove(moves[i])  #если проверка на шах прошла, то удаляет текущий ход из списка возможных                 
+            self.whitetomove = not self.whitetomove
+            self.undo_move() #отменяет фантомный ход
+        if len(moves) == 0: #Мат или Пат
+            if self.check():
+                self.checkmate = True
+            else:
+                self.stalemate = True
+        else:
+            self.checkmate = False
+            self.stalemate = False #возвращаем состояния шаха и мата на случай, если они происходят в "фантомных ходах"
+        return moves+castlemoves
+
+    def check(self): #проверка на шах
+        if self.whitetomove:
+            return self.square_under_attack(self.whiteking_location[0], self.whiteking_location[1])
+        else:
+            return self.square_under_attack(self.blackking_location[0], self.blackking_location[1])
+
+    def square_under_attack(self, row, column): # проверка под атакой ли клетка 
+        self.whitetomove = not self.whitetomove #смена хода для расчета ходов оппонента
+        opponent_moves = self.possiblemoves() #список потенциальных возможных ходов оппонента ПОСЛЕ нашего хода
+        self.whitetomove = not self.whitetomove # после генерации ходов оппонента вернуть ход обратно к себе
+        for move in opponent_moves:
+            if move.second_row == row and move.second_column == column: # данная клетка под атакой одной из фигур оппонента
+                return True
+        return False
+
+    def possiblemoves(self): #возвращает все теоретически возможные ходы для всех фигур на доске
+        moves = []
+        for row in range(8):
+            for column in range(8):
+                color = self.board[row][column][0]
+                if (color == "w" and self.whitetomove) or (color == "b" and not self.whitetomove):
+                    piece = self.board[row][column][1]    
+                    self.move_functions[piece](row, column, moves)
+        return moves
+
+    #функции для ходов каждой из фигур
+    def pawnmoves(self, row, column, moves):#пешка без анпасана и промоушена
+        if self.whitetomove:
+            if self.board[row-1][column] == '--':
+                moves.append(move((row, column),(row-1, column), self.board))
+                if row == 6 and self.board[row-2][column] == '--':
+                    moves.append(move((row, column),(row-2, column), self.board))
+                    #ход вперед+ ход через клетку со стартовой позиции
+            if column-1 >= 0: #проверка левого края борта
+                if self.board[row-1][column-1][0] == 'b':
+                    moves.append(move((row, column),(row-1, column-1), self.board))
+            if column+1 <= 7: #проверка правого края борта
+                if self.board[row-1][column+1][0] == 'b':
+                    moves.append(move((row, column),(row-1, column+1), self.board))
+                
+                #анпасан    
+                #previous_move = self.move_log[-1] if len(self.move_log) != 0 else None
+                #if previous_move.movedpiece == 'bP':
+                #    if row == 3:
+                #        if (previous_move.first_row - row)**2 + (previous_move.first_column - column)**2 == 5:
+                #            moves.append(move((row, column),(row-1, column+1), self.board))
+                #            self.board[row][column+1] = '--'
+        else:# для черных
+            if self.board[row+1][column] == '--':
+                moves.append(move((row, column),(row+1, column), self.board))
+                if row == 1 and self.board[row+2][column] == '--':
+                    moves.append(move((row, column),(row+2, column), self.board))
+                    #ход вперед+ ход через клетку со стартовой позиции
+            if column-1 >= 0: #проверка левого края борта
+                if self.board[row+1][column-1][0] == 'w':
+                    moves.append(move((row, column),(row+1, column-1), self.board))
+            if column+1 <= 7: #проверка правого края борта
+                if self.board[row+1][column+1][0] == 'w':
+                    moves.append(move((row, column),(row+1, column+1), self.board))
+
+    def rookmoves(self, row, column, moves):#Сканирует каждую из сторон
+            opponentcolor = 'b' if self.whitetomove else 'w'
+            alliescolor = 'w' if self.whitetomove else 'b'
+            for i in range(1,row+1):#вверх
+                if self.board[row-i][column][0] == opponentcolor:
+                        moves.append(move((row, column),(row-i, column), self.board))
+                        break
+                elif self.board[row-i][column][0] == alliescolor:
+                        break
+                moves.append(move((row, column),(row-i, column), self.board))
+            for i in range(1,7-row+1):#вниз
+                if self.board[row+i][column][0] == opponentcolor:
+                        moves.append(move((row, column),(row+i, column), self.board))
+                        break
+                elif self.board[row+i][column][0] == alliescolor:
+                        break
+                moves.append(move((row, column),(row+i, column), self.board))
+            for i in range(1,column+1):#влево
+                if self.board[row][column-i][0] == opponentcolor:
+                        moves.append(move((row, column),(row, column-i), self.board))
+                        break
+                elif self.board[row][column-i][0] == alliescolor:
+                        break
+                moves.append(move((row, column),(row, column-i), self.board))
+            for i in range(1,7-column+1):#вправо
+                if self.board[row][column+i][0] == opponentcolor:
+                        moves.append(move((row, column),(row, column+i), self.board))
+                        break
+                elif self.board[row][column+i][0] == alliescolor:
+                        break
+                moves.append(move((row, column),(row, column+i), self.board))
+
+    def knightmoves(self, row, column, moves):#5х5 вокруг себя на теореме пифагора
+        opponentcolor = 'b' if self.whitetomove else 'w'
+        for r in range(row-2, row+3):
+            if 0<=r<=7:
+                for c in range(column-2, column+3):
+                    if 0<=c<=7:     
+                        if (row-r)**2+(column-c)**2 == 5:
+                            if self.board[r][c][0] == opponentcolor or self.board[r][c][0] == '-':
+                                moves.append(move((row, column),(r, c), self.board))
+     
+    def bishopmoves(self, row, column, moves):# питон его знает, в бубен стукнуть надо, чтобы работало
+        opponentcolor = 'b' if self.whitetomove else 'w'
+        alliescolor = 'w' if self.whitetomove else 'b'
+        for i in range(1,7):
+            if 0 <= row-i <= 7 and 0 <= column-i <= 7:
+                if self.board[row-i][column-i][0] == opponentcolor:
+                    moves.append(move((row, column),(row-i, column-i), self.board))
+                    break
+                elif self.board[row-i][column-i][0] == alliescolor:
+                    break
+                moves.append(move((row, column),(row-i, column-i), self.board))
+        
+        for i in range(1,7):
+            if 0 <= row+i <= 7 and 0 <= column+i <= 7:
+                if self.board[row+i][column+i][0] == opponentcolor:
+                    moves.append(move((row, column),(row+i, column+i), self.board))
+                    break
+                elif self.board[row+i][column+i][0] == alliescolor:
+                    break
+                moves.append(move((row, column),(row+i, column+i), self.board))
+
+        for i in range(1,7):
+            if 0 <= row-i <= 7 and 0 <= column+i <= 7:
+                if self.board[row-i][column+i][0] == opponentcolor:
+                    moves.append(move((row, column),(row-i, column+i), self.board))
+                    break
+                elif self.board[row-i][column+i][0] == alliescolor:
+                    break
+                moves.append(move((row, column),(row-i, column+i), self.board))
+        
+        for i in range(1,7):
+            if 0 <= row+i <= 7 and 0 <= column-i <= 7:
+                if self.board[row+i][column-i][0] == opponentcolor:
+                    moves.append(move((row, column),(row+i, column-i), self.board))
+                    break
+                elif self.board[row+i][column-i][0] == alliescolor:
+                    break
+                moves.append(move((row, column),(row+i, column-i), self.board))
+        
+    def queenmoves(self, row, column, moves):#Ладья+слон
+        self.rookmoves(row, column, moves)
+        self.bishopmoves(row, column, moves)
+
+    def kingmoves(self, row, column, moves):#рассматривает 3х3 клетки вокруг себя, добавляет, если пусто или враг + рокировка
+        opponentcolor = 'b' if self.whitetomove else 'w'
+        for r in range(row-1, row+2):
+            if 0<=r<=7:
+                for c in range(column-1, column+2):
+                    if 0<=c<=7:     
+                        if self.board[r][c][0] == opponentcolor or self.board[r][c][0] == '-':
+                            moves.append(move((row, column),(r, c), self.board))
+
+    def castlemoves(self, row, column):
+        castlemoves = []
+        if self.whitetomove:
+            if self.kingsidecastle_white:
+                if not self.check():                       #---------------------------------------------------------
+                    if self.board[row][column+1] == '--' and self.board[row][column+2] == '--':
+                        if not self.square_under_attack(row, column+1) and not self.square_under_attack(row, column+2):
+                           castlemoves.append(move((row, column),(row, column+2), self.board, castling=True))#не двигает ладью
+        return castlemoves
+
+        
+
+class move():# Первая и вторая нажатые клетки, двинутая и съеденая фигура (не работает с анпасан, рокировкой и промоушеном)
+    def __init__(self, first_SQ, second_SQ, board, castling=False): #откуда(1,1), куда(2,2), gamestate.board
+        self.first_row = first_SQ[0]
+        self.first_column = first_SQ[1]
+        self.second_row = second_SQ[0]
+        self.second_column = second_SQ[1]
+        self.movedpiece = board[self.first_row][self.first_column]
+        self.capturedpiece = board[self.second_row][self.second_column]
+        self.moveID = self.first_row * 1000 + self.first_column * 100 + self.second_row * 10 + self.second_column
+        # moveID - уникальный идентификатор хода из клетки 1 в клетку 2
+        self.castling=castling
+        
+    
+    def __eq__(self, other):
+        if isinstance(other, move):
+            return self.moveID == other.moveID
+    #питон не может сравнить два объекта класс, он видит каждый ход(объект класс) как уникальные
+    #объекты, поэтому дополняем базовый метод сравнения, ходы он будет сравнивать через moveID
+
+    ranks_to_rows = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0,}
+    rows_to_ranks = {value: key for key, value in ranks_to_rows.items()}
+    files_to_columns = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7,}
+    columns_to_files = {value: key for key, value in files_to_columns.items()}
+    # библиотеки для перевода координат из board в стандартные шахматные координаты
+    # board[1][1]->b7
+    def get_rankfile(self, row, column): #возвращает строку с кординатой клетки 'e2'
+        return self.columns_to_files[column] + self.rows_to_ranks[row]
+
+    def get_movenotation(self): #Дальше реализовать тут все общепринятые правила записи ходов
+        return self.get_rankfile(self.first_row, self.first_column) + self.get_rankfile(self.second_row, self.second_column)
+    
+    
+    
+    
+    #анпасан, кастл,
---
+++ chess_main.py
@@ -0,0 +1,130 @@
+###       Input and display        ###
+
+import pygame
+import chess_engine
+pygame.init()
+
+WIDTH = HEIGHT = 848
+SQ_SIZE = WIDTH//8
+screen = pygame.display.set_mode((WIDTH+WIDTH*0.1, HEIGHT+WIDTH*0.1))
+Game_State = chess_engine.Game_State()
+FPS = 30  
+clock = pygame.time.Clock()
+
+WHITE_SQ = (104, 113, 130)
+DARK_SQ = (41, 46, 59)
+BORDER_COLOR = (37, 40, 53)
+SQ_BORDER_COLOR = (200, 200, 200)
+
+images = {}
+def load_imgages(): #запускается 1 раз до главного цикла while
+    pieces = ("bB", "bK", "bN", "bP", "bQ", "bR", "wB", "wK", "wN", "wP", "wQ", "wR")
+    for piece in pieces:
+        images[piece] = pygame.transform.scale(pygame.image.load(f'chess/images/{piece}.png'), (SQ_SIZE,SQ_SIZE))
+# библиотека key = называние фигуры, value = путь до картинки
+
+
+
+def draw_board(screen):
+    pygame.draw.rect(screen, BORDER_COLOR, (0, 0, WIDTH+WIDTH*0.1, HEIGHT+WIDTH*0.1))
+    pygame.draw.rect(screen, WHITE_SQ, (0+WIDTH*0.05, 0+WIDTH*0.05, WIDTH, HEIGHT))
+    for column in range(8):
+        for row in range(0, 8, 2):
+                if column % 2 != 0:
+                    pygame.draw.rect(screen, DARK_SQ, (row*SQ_SIZE+WIDTH*0.05, column*SQ_SIZE+WIDTH*0.05, SQ_SIZE, SQ_SIZE))
+                else:
+                    pygame.draw.rect(screen, DARK_SQ, (row*SQ_SIZE+SQ_SIZE+WIDTH*0.05, column*SQ_SIZE+WIDTH*0.05, SQ_SIZE, SQ_SIZE))
+    
+
+def draw_pieces(screen, board):
+    for row in range(8):
+        for column in range(8):
+            piece = board[row][column]
+            if piece != "--":
+                screen.blit(images[piece], (column*SQ_SIZE+WIDTH*0.05, row*SQ_SIZE+WIDTH*0.05))
+                #вызывает key из библиотеки images, возвращает картинку фигуры по координатам
+
+def draw_gamestate(screen, Game_State):
+    draw_board(screen)
+    draw_pieces(screen, Game_State.board)
+
+def active_SQ(x, y):
+    for row in range(8):
+        for column in range(8):
+            if column*SQ_SIZE+WIDTH*0.05<x<column*SQ_SIZE+WIDTH*0.05+SQ_SIZE and row*SQ_SIZE+WIDTH*0.05<y<row*SQ_SIZE+WIDTH*0.05+SQ_SIZE:
+                square = (column*SQ_SIZE+WIDTH*0.05, row*SQ_SIZE+WIDTH*0.05)
+                pygame.draw.rect(screen, SQ_BORDER_COLOR, (square[0], square[1], SQ_SIZE, SQ_SIZE), 2)
+    
+def active_piece(row, column):
+    piece = Game_State.board[row][column]
+    moves = []
+    if (piece[0] == 'w' and Game_State.whitetomove) or (piece[0] == 'b' and not Game_State.whitetomove) and piece[1] != '-':
+        Game_State.move_functions[piece[1]](row, column, moves)
+        pygame.draw.rect(screen, SQ_BORDER_COLOR, (column*SQ_SIZE+WIDTH*0.05, row*SQ_SIZE+WIDTH*0.05, SQ_SIZE, SQ_SIZE), 2)
+    moves = Game_State.validmoves(moves)
+    for i in moves:
+        pygame.draw.rect(screen, SQ_BORDER_COLOR, (i.second_column*SQ_SIZE+WIDTH*0.05, i.second_row*SQ_SIZE+WIDTH*0.05, SQ_SIZE, SQ_SIZE), 2)
+ #def main()
+ #  if __name__ == "__main__":  
+ #      main()   
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""
+load_imgages()
+draw_gamestate(screen, Game_State)
+Game_State.validmoves(Game_State.possiblemoves()) #список легальных ходов для начала партии
+move_was_made = False #флаг для вычисления легальных ходов только после совершения хода
+
+running = True
+mouse_position = (0, 0) #стартовая позиция мыши для active_SQ
+last_click = () #(row, col)
+player_clicks = [] # [(row, col),(row, col)]
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEMOTION:
+            mouse_position = event.pos
+            #print(mouse_position)
+        elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    Game_State.undo_move()
+                    move_was_made = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            row = (mouse_position[1]-int(WIDTH*0.05))//SQ_SIZE
+            column = (mouse_position[0]-int(WIDTH*0.05))//SQ_SIZE
+            if -1 < row < 8 and -1 < column < 8:
+                last_click = (row, column)
+                player_clicks.append(last_click)
+            if len(player_clicks) == 2:
+                if player_clicks[0] != player_clicks[1]:
+                    move = chess_engine.move(player_clicks[0], player_clicks[1], Game_State.board)
+                    if move in Game_State.validmoves(Game_State.possiblemoves()):
+                        Game_State.make_move(move)
+                        move_was_made = True
+                        last_click = ()
+                        player_clicks = []
+                        print(move.get_movenotation())
+                        print(Game_State.castle_log[-1])
+                    else:
+                        last_click = (row, column)
+                        player_clicks = []
+                        player_clicks.append(last_click)
+                else:
+                    last_click = ()
+                    player_clicks = []
+                    #print("canceled")
+                    #Дважды нажатая одна и та же клетка не произведет хода, действие мышкой отменяется
+            if move_was_made:
+                validmoves = Game_State.validmoves(Game_State.possiblemoves())
+                move_was_made = False
+    draw_gamestate(screen, Game_State)
+    if len(player_clicks) == 1:
+        active_piece(last_click[0], last_click[1])
+    
+    #active_SQ(mouse_position[0], mouse_position[1])
+    clock.tick(FPS)
+    #Функция clock.tick() приостанавливает выполнение кода до тех пор, пока не пройдет достаточно времени, чтобы достичь нужной частоты кадров.
+    
+    pygame.display.flip()
+    
+
